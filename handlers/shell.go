c9cb2cd8f81737c74baabc7ed48adfa320dabe11
David Gageot <david@gageot.net>
2019-11-24 16:39:01 +0100

    Remove duplication
    Signed-off-by: David Gageot <david@gageot.net>

--- handlers/shell.go
+++ handlers/shell.go
@@ -39,42 +39,12 @@ func Shell(w http.ResponseWriter, r *http.Request) {
 		path += "/" + folder
 	}
 
-	commands := []string{"cd " + path}
-
-	shell, found := os.LookupEnv("SHELL")
-	if !found {
-		shell = "bash"
-	}
-	fmt.Println("Using shell", shell)
-
-	bashRc, bashRcErr := getBashRcFile()
-	if bashRcErr != nil {
-		fmt.Println(bashRcErr)
-		http.Error(w, bashRcErr.Error(), 500)
+	commands, err := commands(path)
+	if err != nil {
+		fmt.Println(err)
+		http.Error(w, err.Error(), 500)
 		return
 	}
-	if bashRc != "" {
-		fmt.Println("Using bashrc file", bashRc)
-
-		commands = append(commands, fmt.Sprintf("source %s", bashRc))
-	}
-
-	bashHistory, bashHistoryErr := getBashHistoryCopy()
-	if bashHistoryErr != nil {
-		fmt.Println(bashHistoryErr)
-		http.Error(w, bashHistoryErr.Error(), 500)
-		return
-	}
-	if bashHistory != "" {
-		fmt.Println("Using history", bashHistory)
-
-		commands = append(commands, fmt.Sprintf("HISTFILE=%s exec %s", bashHistory, shell))
-	} else {
-		commands = append(commands, fmt.Sprintf("exec %s", shell))
-	}
-
-	parameters := url.Values{}
-	parameters.Set("arg", strings.Join(commands, ";"))
 
 	domain := "localhost"
 	if referer := r.Header.Get("Referer"); referer != "" {
@@ -83,51 +53,63 @@ func Shell(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 
+	parameters := url.Values{}
+	parameters.Set("arg", strings.Join(commands, ";"))
 	url := fmt.Sprintf("http://%s:%d/?%s", domain, *flags.ShellPort, parameters.Encode())
+
 	http.Redirect(w, r, url, 303)
 }
 
-func getBashHistoryCopy() (string, error) {
-	history, err := files.Read(".demoit", ".bash_history")
+func commands(path string) ([]string, error) {
+	commands := []string{"cd " + path}
+
+	shell, found := os.LookupEnv("SHELL")
+	if !found {
+		shell = "bash"
+	}
+	fmt.Println("Using shell", shell)
+
+	bashRc, err := copyFile(".bashrc")
 	if err != nil {
-		if errors.Is(err, os.ErrNotExist) {
-			// Ignore silently
-			return "", nil
-		}
-		return "", fmt.Errorf("Unable to read bash history: %w", err)
+		return nil, err
+	}
+	if bashRc != "" {
+		fmt.Println("Using bashrc file", bashRc)
+		commands = append(commands, fmt.Sprintf("source %s", bashRc))
 	}
 
-	tmpFile, err := ioutil.TempFile("", "demoit")
+	bashHistory, err := copyFile(".bash_history")
 	if err != nil {
-		return "", fmt.Errorf("Unable to create temp file for bash history: %w", err)
+		return nil, err
+	}
+	if bashHistory != "" {
+		fmt.Println("Using history", bashHistory)
+		commands = append(commands, fmt.Sprintf("HISTFILE=%s exec %s", bashHistory, shell))
+	} else {
+		commands = append(commands, fmt.Sprintf("exec %s", shell))
 	}
 
-	_, err = tmpFile.Write(history)
-	if err != nil {
-		return "", fmt.Errorf("Unable to write bash history: %w", err)
-	}
-
-	return tmpFile.Name(), nil
+	return commands, nil
 }
 
-func getBashRcFile() (string, error) {
-	bashrc, err := files.Read(".demoit", ".bashrc")
+func copyFile(file string) (string, error) {
+	content, err := files.Read(".demoit", file)
 	if err != nil {
 		if errors.Is(err, os.ErrNotExist) {
 			// Ignore silently
 			return "", nil
 		}
-		return "", fmt.Errorf("Unable to read bashrc file: %w", err)
+		return "", fmt.Errorf("Unable to read file %s: %w", file, err)
 	}
 
 	tmpFile, err := ioutil.TempFile("", "demoit")
 	if err != nil {
-		return "", fmt.Errorf("Unable to create temp file for bash history: %w", err)
+		return "", fmt.Errorf("Unable to create temp file: %w", err)
 	}
 
-	_, err = tmpFile.Write(bashrc)
+	_, err = tmpFile.Write(content)
 	if err != nil {
-		return "", fmt.Errorf("Unable to write bashrc file: %w", err)
+		return "", fmt.Errorf("Unable to write file: %w", err)
 	}
 
 	return tmpFile.Name(), nil
