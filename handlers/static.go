4f9bda99cbd7ebeab8a382aca348d2b13672fb8e
David Gageot <david.gageot@docker.com>
2026-03-20 07:48:27 +0100

    handlers/static: create http.FileServer once instead of per-request
    The Static handler was allocating a new http.Dir and http.FileServer on
    every incoming request. Use sync.Once to create the file server lazily
    on first use, avoiding repeated allocations.
    
    Assisted-By: docker-agent

--- handlers/static.go
+++ handlers/static.go
@@ -3,13 +3,21 @@ package handlers
 import (
 	"net/http"
 	"path/filepath"
+	"sync"
 
 	"github.com/dgageot/demoit/files"
 )
 
+var (
+	staticOnce   sync.Once
+	staticServer http.Handler
+)
+
 // Static renders static files.
 func Static(w http.ResponseWriter, r *http.Request) {
-	fs := http.Dir(filepath.Join(files.Root, ".demoit"))
+	staticOnce.Do(func() {
+		staticServer = http.FileServer(http.Dir(filepath.Join(files.Root, ".demoit")))
+	})
 
-	http.FileServer(fs).ServeHTTP(w, r)
+	staticServer.ServeHTTP(w, r)
 }
